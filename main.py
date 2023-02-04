a8a6f9260d6372371ba4bd4b9feee5c236aef3a1
SouhaSoukane <ks_soukane@esi.dz>
2023-02-04 20:18:20 +0100

    delete old files

--- main.py
+++
@@ -1,4 +0,0 @@
-from web_app import create_app
-app=create_app()
-if __name__=="__main__":
- app.run(port=7000,debug=True)
--- web_app/auth.py
+++
@@ -1,4 +0,0 @@
-from os import abort
-from flask import Blueprint, redirect, session, url_for, request
-from flask_login import login_user, login_required, logout_user, current_user, LoginManager
-
